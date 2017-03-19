577300f80f9a1be0263a1d499065838ad2c2e173
Louis Salin <Louis Salin>
2017-03-19 16:48:58 -0500

    got an angular app running

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,3 +1,12 @@
+///<reference path="../../typings/index.d.ts"/>
+
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {AppModule} from './app.module';
+
+const platform = platformBrowserDynamic();
+
+platform.bootstrapModule(AppModule);
+
 function sayHello(name: string){
     return `Hello from ${name}`;
 }
